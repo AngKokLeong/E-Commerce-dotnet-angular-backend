9ea37da48d10c20a3caee38e749afacb73cddb6f
Ang Kok Leong <>
2023-11-01 09:44:20 +0000

    SNEC-17 added Product entity class

---
+++ API/Entities/Product.cs
@@ -0,0 +1,16 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+    }
+
+}
