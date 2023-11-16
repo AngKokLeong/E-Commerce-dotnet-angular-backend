9debc2f48adfd3a631f9f76c916df50d503032dd
Ang Kok Leong <>
2023-11-16 05:18:33 +0000

    SNEC-26 extend BaseEntity in Product class

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -2,13 +2,9 @@
 namespace Core.Entities
 {
 
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
-
-        
     }
 
 }
